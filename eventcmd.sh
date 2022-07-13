ec7bee48fa98334089622521f08058d8883dcf17
Tom Cooper <tomcooper@tec8.net>
2022-07-13 13:55:46 -0700

    clean up - use /proc file

--- eventcmd.sh
+++ eventcmd.sh
@@ -1,24 +1,23 @@
-#todo: test ability to pass environment variable RECORD FOLDER - should be able to just use basename on 
+#todo: test ability to pass environment variable RECORD FOLDER - should be able to just use dirname on 
 # full path retrieved from proc environment
 
-new_proc=-11
-last_file=unknown_file
-last_title=unknown_title
-last_title=`cat "${XDG_HOME_CONFIG:-${HOME}/.config}/pianobar/last_title"`
+
 rec_pid=`cat "${XDG_HOME_CONFIG:-${HOME}/.config}/pianobar/record_pid"`
-echo last title: $last_title
+echo record process: $rec_pid
 
 case "$1" in
 	songfinish)
 
-		last_file=`ls -t1  /home/debian/thesmiths/test*wav | head -1`
-		proc_current_file=`readlink /proc/$rec_pid/fd/3`
-		echo last: $last_file
-		echo last proc: $proc_current_file 
-		kill -s SIGUSR1 $rec_pid 
-		mv $last_file "/home/debian/thesmiths/$last_title.wav"
+		
+		proc_last_file=`readlink /proc/$rec_pid/fd/3`
+		echo current file: $proc_last_file 
+		kill -s SIGUSR1 $rec_pid
+		path=$(dirname $proc_current_file)
+		#fix titlename - use new function
+		#fixed_title=`echo -n $title | tr [:space:] - | tr  /\' -`
+		mv $proc_last_file "$path/$fixed_title.wav"
 		
 		echo "$title -- $artist" > "${XDG_HOME_CONFIG:-${HOME}/.config}/pianobar/nowplaying"
-		tr_title=`echo -n $title | tr [:space:] - | tr  /\' -`
-		echo "$tr_title" > "${XDG_HOME_CONFIG:-${HOME}/.config}/pianobar/last_title"
+		
+		
 		
